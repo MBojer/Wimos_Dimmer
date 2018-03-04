5ed2fe146a8767db84f1e72c808854db91ee08f4
Bojer <morten.bojer@accenture.com>
2018-03-04 15:57:18 +0000

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,247 @@
+/*
+Pins
+
+  D0 = 16
+  D1 = 5 - Dimmer 1
+  D2 = 4 - Dimmer 2
+  D3 = Not used
+  D4 =  Not used (BUILTIN_LED)
+  D5 = 14 - Dimmer 3
+  D6 = 12 - Dimmer 4
+  D7 = 13 - Dimmer 5
+  D8 = 15
+
+
+WiFi.status() =
+  0 : WL_IDLE_STATUS when Wi-Fi is in process of changing between statuses
+  1 : WL_NO_SSID_AVAILin case configured SSID cannot be reached
+  3 : WL_CONNECTED after successful connection is established
+  4 : WL_CONNECT_FAILED if password is incorrect
+  6 : WL_DISCONNECTED if module is not configured in station mode
+*/
+
+#include <Arduino.h>
+
+// ---------------------------------------- WiFi ----------------------------------------
 #include <ESP8266WiFi.h>
 
-const char* ssid = "NoInternetHere";
-// const char* ssid = "NoInternetHereEither";
-const char* password = "NoPassword1!";
+const char* WiFi_SSID = "NoInternetHere";
+const char* WiFi_Password = "NoPassword1!";
+String WiFi_Hostname = "Dimmer";
 
-const int Dimmer_1 = 5;
-const int Dimmer_2 = 4;
-const int Dimmer_3 = 14;
-const int Dimmer_4 = 12;
-const int Dimmer_5 = 13;
+unsigned long WiFi_Timeout = 5000; // ms before marking connection as failed
+unsigned long WiFi_Reconnect_At = 0;
+unsigned long WiFi_Reconnect_Delay = 10000; // ms before marking connection as failed
 
-unsigned int Light_Strength = 0;
+const int Dimmer_Number_Of = 5;
+const int Dimmer_Pin[Dimmer_Number_Of] {D1, D2, D5, D6, D7};
 
-unsigned int Light_Strength_Procent = 0;
-
-byte Strenght_Dimmer_1 = 0;
-byte Strenght_Dimmer_2 = 0;
-byte Strenght_Dimmer_3 = 0;
-byte Strenght_Dimmer_4 = 0;
-byte Strenght_Dimmer_5 = 0;
+byte Dimmer_Strenght[Dimmer_Number_Of];
 
 
-byte Fade_Jump = 25;
 
-byte WiFi_Connected = false;
 
+// ---------------------------------------- ??? ----------------------------------------
 WiFiServer server(80);
+WiFiClient client;
 
 
-void Start_Web_Server() {
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
+// ---------------------------------------- HTTP_GET() ----------------------------------------
+String HTTP_GET(String Server, int Port, String URL) {
 
-  // Print the IP address
-  Serial.print("Use this URL : ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
+  // Attempt to make a connection to the remote server
+  if ( !client.connect(Server, Port) ) {
+    return "Connection failed";
+  }
+
+  // This will send the request to the server
+  client.print(String("GET /") + URL + " HTTP/1.1\r\n" +
+               "Host: " + Server + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(50);
+
+  // Read all the lines of the reply from server and print them to Serial
+  String Reply;
+  while(client.available()){
+    Reply += client.readStringUntil('\r');
+  }
+
+  return Reply;
 }
 
-void i2c_Test() {
 
-  Serial.println("MARKER");
+// ---------------------------------------- Web_Server() ----------------------------------------
+void Web_Server() {
 
-} // i2c_Test
+  if (server.status() == false) {
+    if (WiFi.status() == 3) {
+      // Start the server
+      server.begin();
+      Serial.println("Server started");
 
-
-void WiFi_Connect() {
-
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+      // Print the IP address
+      Serial.print("Use this URL : ");
+      Serial.print("http://");
+      Serial.print(WiFi.localIP());
+      Serial.println("/");
+    }
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+
+  else if (server.status() == true) {
+    if (WiFi.status() != 3) {
+        server.stop();
+    }
+  }
+} // Web_Server()
+
+
+
+// ---------------------------------------- WiFi_Reset() ----------------------------------------
+void WiFi_Reset() {
+  Serial.println("Erasing WiFi config");
+  ESP.eraseConfig();
+  delay(1500);
+
+
+  Serial.println("Configuring WiFi");
+  WiFi.enableAP(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoConnect(false);
+  WiFi.setAutoReconnect(false);
+  WiFi.softAPdisconnect(true);
+  WiFi.hostname(WiFi_Hostname);
+}
+
+
+// ---------------------------------------- WiFi_Connect() ----------------------------------------
+bool WiFi_Connect(const char* SSID, const char* Password) {
+
+  byte WiFi_State = WiFi.status();
+
+  // 3 : WL_CONNECTED after successful connection is established
+  // Aparantly someone wanted to reconnecto so resetting and connecting again
+  if (WiFi_State == 3) {
+    Serial.print("Disconnecting from: ");
+    Serial.println(WiFi.SSID());
+    WiFi_Reset();
+  }
+
+  Serial.print("Current WiFi status: ");
+  Serial.println(WiFi.status());
+
+  Serial.print("Connecting to: ");
+  Serial.print(SSID);
+  Serial.print(" ");
+
+  unsigned long WiFi_Connect_Start = millis() + WiFi_Timeout;
+
+  WiFi.begin(SSID, Password);
+
+  while (WiFi.status() != 3) {
+    if (WiFi_Connect_Start < millis()) {
+      Serial.println(" connection timeout.");
+      return false;
+    }
+    Serial.print(".");
+    delay (500);
+  }
+
+  Serial.println(" connected.");
+  Web_Server();
+  return true;
 
 
 
 
 } // WiFi_Connect
 
+// ---------------------------------------- WiFi_Check() ----------------------------------------
 
 void WiFi_Check() {
 
-  if (WiFi.status() == 3 & WiFi_Connected != true) {
 
-    WiFi_Connected = true;
+  if (WiFi_Reconnect_At > millis()) { // Wait for timer to expire
+    return;
+  }
+
+  byte WiFi_State = WiFi.status();
+
+  //   0 : WL_IDLE_STATUS when Wi-Fi is in process of changing between statuses
+  if (WiFi_State == 0) {
+    Serial.println("WiFi Idle");
+    if (WiFi.gatewayIP().toString() == "0.0.0.0") { // No gateway assuming WiFi has lost connection
+      Serial.println("No gateway, assuming WiFi has lost connection, resetting WiFi.");
+      WiFi_Reset();
+      WiFi_Connect(WiFi_SSID, WiFi_Password);
+    }
+  }
+
+  // 1 : WL_NO_SSID_AVAILin case configured SSID cannot be reached
+  else if (WiFi_State == 1) {
+    Serial.print("SSIID: ");
+    Serial.print(WiFi.SSID());
+    Serial.println(" - Out of range.");
+  }
+
+  // 3 : WL_CONNECTED after successful connection is established
+  else if (WiFi_State == 3) {
+
+    if (WiFi.gatewayIP().toString() == "0.0.0.0") { // No gateway assuming WiFi has lost connection
+      Serial.println("No gateway, assuming WiFi has lost connection, resetting WiFi.");
+      WiFi_Reset();
+      WiFi_Connect(WiFi_SSID, WiFi_Password);
+    }
+
+    else {
+      Serial.println("WiFi Connected.");
+      Web_Server();
+    }
 
   }
 
+  // 4 : WL_CONNECT_FAILED if password is incorrect
+  else if (WiFi_State == 4) {
+    Serial.print("Password incorrect for SSID: ");
+    Serial.println(WiFi.SSID());
+    WiFi_Reset();
+    WiFi_Connect(WiFi_SSID, WiFi_Password);
+  }
 
-  if (WiFi.status() != 3) {
+  // 6 : WL_DISCONNECTED if module is not configured in station mode
+  else if (WiFi_State == 6) {
+    Serial.println("WiFi not configured");
+    WiFi_Connect(WiFi_SSID, WiFi_Password);
+  }
 
-    Serial.print("WiFi.status(): ");
+  else {
+    Serial.print("Current WiFi status: ");
     Serial.println(WiFi.status());
-
-     WiFi_Connected = false;
-
   }
 
-  if (WiFi_Connected == false) {
-    WiFi_Connect();
-    Start_Web_Server();
-  }
-
+  WiFi_Reconnect_At = millis() + WiFi_Reconnect_Delay;
 }
-
 
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println();
+  Serial.println("Booting");
 
-  pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(Dimmer_1, OUTPUT);
-  pinMode(Dimmer_2, OUTPUT);
-  pinMode(Dimmer_3, OUTPUT);
-  pinMode(Dimmer_4, OUTPUT);
-  pinMode(Dimmer_5, OUTPUT);
-
-
-  analogWrite(Dimmer_1, 0);
-  analogWrite(Dimmer_2, 0);
-  analogWrite(Dimmer_3, 0);
-  analogWrite(Dimmer_4, 0);
-  analogWrite(Dimmer_5, 0);
-
-
+  for (byte i = 0; i < Dimmer_Number_Of; i++) {
+    pinMode(Dimmer_Pin[i], OUTPUT);
+    analogWrite(Dimmer_Pin[i], 0);
+    Dimmer_Strenght[i] = 0;
+  }
 
   WiFi_Check();
 
-
-
+  Serial.println("Boot done");
 }
 
 void loop() {
 
   WiFi_Check();
-
-
 
 
   // Check if a client has connected
@@ -144,75 +257,46 @@ void loop() {
 
   // Read the first line of the request
   String request = client.readStringUntil('\r');
-  Serial.println(request);
   client.flush();
 
   // Match the request
 
-  // remove "/light" and set light based on % number in get requist
-
   if (request.indexOf("GET /Dimmer_") != -1) {
+
+    unsigned int Light_Strength = 0;
+    unsigned int Light_Strength_Procent = 0;
 
     request.replace("GET /", "");
     request.replace(" HTTP/1.1", "");
 
-    // don't remove "Dimmer_" and make define statements matching dimmer_1 to something then use them to set pinf numbers, not possible it seems
+    String Temp_String = request.substring(0, request.indexOf("-"));
 
-    String Selected_Dimmer = request.substring(0, request.indexOf("-"));
+    Temp_String.replace("Dimmer_", "");
+
+    int Selected_Dimmer = Temp_String.toInt();
 
     Light_Strength_Procent = request.substring(request.indexOf("-") + 1, request.length()).toInt();
 
     Light_Strength = (Light_Strength_Procent * 0.01) * 1023;
-    Serial.print("Light_Strength: ");
-    Serial.println(Light_Strength);
 
-    if (Selected_Dimmer == "Dimmer_1") {
-      analogWrite(Dimmer_1, Light_Strength);
-      Strenght_Dimmer_1 = Light_Strength_Procent;
-      Serial.print("Dimmer 1: ");
-      Serial.println(Light_Strength);
+
+    if (Selected_Dimmer <= Dimmer_Number_Of) {
+      analogWrite(Dimmer_Pin[Selected_Dimmer - 1], Light_Strength);
+      Dimmer_Strenght[Selected_Dimmer - 1] = Light_Strength_Procent;
+      Serial.print("Dimmer ");
+      Serial.print(Selected_Dimmer);
+      Serial.print(": ");
+      Serial.println(Dimmer_Strenght[Selected_Dimmer]);
     }
-    else if (Selected_Dimmer == "Dimmer_2") {
-      analogWrite(Dimmer_2, Light_Strength);
-      Strenght_Dimmer_2 = Light_Strength_Procent;
-      Serial.print("Dimmer 2: ");
-      Serial.println(Light_Strength);
-    }
-    else if (Selected_Dimmer == "Dimmer_3") {
-      analogWrite(Dimmer_3, Light_Strength);
-      Strenght_Dimmer_3 = Light_Strength_Procent;
-      Serial.print("Dimmer 3: ");
-      Serial.println(Light_Strength);
-    }
-    else if (Selected_Dimmer == "Dimmer_4") {
-      analogWrite(Dimmer_4, Light_Strength);
-      Strenght_Dimmer_4 = Light_Strength_Procent;
-      Serial.print("Dimmer 4: ");
-      Serial.println(Light_Strength);
-    }
-    else if (Selected_Dimmer == "Dimmer_5") {
-      analogWrite(Dimmer_5, Light_Strength);
-      Strenght_Dimmer_5 = Light_Strength_Procent;
-      Serial.print("Dimmer 5: ");
-      Serial.println(Light_Strength);
-    }
-    else if (Selected_Dimmer == "Dimmer_99") {
-      analogWrite(Dimmer_1, 0);
-      Strenght_Dimmer_1 = 0;
-      analogWrite(Dimmer_2, 0);
-      Strenght_Dimmer_2 = 0;
-      analogWrite(Dimmer_3, 0);
-      Strenght_Dimmer_3 = 0;
-      analogWrite(Dimmer_4, 0);
-      Strenght_Dimmer_4 = 0;
-      analogWrite(Dimmer_5, 0);
-      Strenght_Dimmer_5 = 0;
+
+    else if (Selected_Dimmer == 99) {
+      for (byte i = 0; i < Dimmer_Number_Of; i++) {
+          analogWrite(Dimmer_Pin[i], 0);
+          Dimmer_Strenght[i] = 0;
+      }
       Serial.println("All OFF");
     }
-  }
 
-  else if (request.indexOf("GET /i2c") != -1) {
-    i2c_Test();
   }
 
   else Serial.println(request);
@@ -227,57 +311,14 @@ void loop() {
   client.println("Wemos Dimmer v2.0");
   client.println("<br><br>");
 
-  client.print("Dimmer 1: ");
-  client.print(Strenght_Dimmer_1);
-  client.println("&#37<br>");
+  for (byte i = 1; i <= Dimmer_Number_Of; i++) {
+    client.print("Dimmer ");
+    client.print(i);
+    client.print(": ");
+    client.print(Dimmer_Strenght[i - 1]);
+    client.println("&#37<br>");
+  }
 
-  client.print("Dimmer 2: ");
-  client.print(Strenght_Dimmer_2);
-  client.println("&#37<br>");
-
-  client.print("Dimmer 3: ");
-  client.print(Strenght_Dimmer_3);
-  client.println("&#37<br>");
-
-  client.print("Dimmer 4: ");
-  client.print(Strenght_Dimmer_4);
-  client.println("&#37<br>");
-
-  client.print("Dimmer 5: ");
-  client.print(Strenght_Dimmer_5);
-  client.println("&#37<br>");
-
-  client.println("<br><br>");
-  client.println("<a href=\"/i2c\">i2c Test");
-
-
-  //
-  //
-  // if (Light_Strength == 0) {
-  //   client.print("0 procent");
-  // }
-  // else if (Light_Strength == 767) {
-  //   client.print("25 procent");
-  // }
-  // else if (Light_Strength == 512) {
-  //   client.print("50 procent");
-  // }
-  // else if (Light_Strength == 255) {
-  //   client.print("75 procent");
-  // }
-  // else if (Light_Strength == 255) {
-  //   client.print("100 procent");
-  // }
-  //
-  //
-  // client.println("<br><br>");
-  // client.println("<a href=\"/Light=25\">Change light strenght to: 25 procent");
-  // client.println("<br><br>");
-  // client.println("<a href=\"/Light=50\">Change light strenght to: 50 procent");
-  // client.println("<br><br>");
-  // client.println("<a href=\"/Light=75\">Change light strenght to: 75 procent");
-  // client.println("<br><br>");
-  // client.println("<a href=\"/Light=100\">Change light strenght to: 100 procent");
   client.println("</html>");
 
   delay(1);
